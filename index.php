9bb7e5901e3a22acfe71f7edd89b86ea7d934c96
BLANDIN Arthur <blandin.arthur20@gmail.com>
2022-06-13 21:47:37 +0200

    Responsive côté écrans PC + PC portable

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $nbPacks = count($resultPacks);
 <link rel="stylesheet" href="css/style.css">
 
 </head>
-<body class="bgfontblack">
+<body class="bgfontblack resize">
 
   <div class="loader">
         <span class="lettre">C</span>
@@ -60,9 +60,6 @@ $nbPacks = count($resultPacks);
           <h4><i class="textcolor pt-5 pb-2">Trajets</i></h4>
           <h1 class="textcolor pb-2">Nous vous proposons</h1>
           </div>
-          <!-- <div class="row py-3 px-5"> -->
-
-
 
           <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
@@ -70,14 +67,13 @@ $nbPacks = count($resultPacks);
               <?php for ($t = 1; $t < $nbPacks; $t++) { ?>
               <li data-target="#carouselExampleIndicators" data-slide-to="<?php print_r($resultPacks[$t]["id"]); ?>"></li>
               <?php } ?>
-              <!-- <li data-target="#carouselExampleIndicators" data-slide-to="2"></li> -->
             </ol>
             <div class="carousel-inner pt-5">
             <?php for ($t = 1; $t < $nbPacks; $t++) { ?>
 
               <?php if($t == 1) {?>
               <div class="carousel-item active">
-              <a href='offer_details.php?name=<?php print_r($resultPacks[$t-1]["name"]); ?>'><img class="d-flex justify-content-center" src="img/<?php print_r(strtolower(str_replace(' ','',$resultPacks[$t-1]["name"]))); ?>.jpg" width="40%"></a>
+              <a href='offer_details.php?name=<?php print_r($resultPacks[$t-1]["name"]); ?>'><img class="d-flex justify-content-center carroussel_img_resize" src="img/<?php print_r(strtolower(str_replace(' ','',$resultPacks[$t-1]["name"]))); ?>.jpg"></a>
                 <div class="carousel-caption d-none d-md-block">
                   <h5><?php echo($resultPacks[$t-1]["name"]); ?></h5>
                   <p><?php echo($resultPacks[$t-1]["description"]); ?></p>
@@ -86,7 +82,7 @@ $nbPacks = count($resultPacks);
               <?php } ?>
               
               <div class="carousel-item">
-                <a href='offer_details.php?name=<?php print_r($resultPacks[$t]["name"]); ?>'><img class="d-flex justify-content-center" src="img/<?php print_r(strtolower(str_replace(' ','',$resultPacks[$t]["name"]))); ?>.jpg" width="40%"></a>
+                <a href='offer_details.php?name=<?php print_r($resultPacks[$t]["name"]); ?>'><img class="d-flex justify-content-center carroussel_img_resize" src="img/<?php print_r(strtolower(str_replace(' ','',$resultPacks[$t]["name"]))); ?>.jpg"></a>
                 <div class="carousel-caption d-none d-md-block">
                   <h5><?php echo($resultPacks[$t]["name"]); ?></h5>
                   <p><?php echo($resultPacks[$t]["description"]); ?></p>
@@ -103,49 +99,18 @@ $nbPacks = count($resultPacks);
               <span class="sr-only">Next</span>
             </a>
           </div>
-
-
-            <!-- <div class="col border_col_left border_col_right">
-              <div class="card bgfontblack">
-                <img class="card-img-top rounded" src="./img/slow.jpg" alt="Card image cap" height="500">
-                <div class="card-body">
-                  <h2 class="card-title textcolor">Forfait Slow</h2>
-                  <i>
-                    <p class="card-text textcolor pb-4">Pour les adeptes dont l'expérience n'est plus à prouver</p>
-                  </i>
-                  <a href="offer_details.php?name=Slow" class="btn btn-success">Plus de détails sur cette offre</a>
-                </div>
-              </div>
-            </div>
-
-
-            <div class="col border_col_right">
-              <div class="card bgfontblack">
-                <img class="card-img-top rounded" src="./img/speed.jpg" alt="Card image cap" height="500">
-                <div class="card-body">
-                  <h2 class="card-title textcolor">Forfait Speed</h2>
-                  <i>
-                    <p class="card-text textcolor pb-4">Pour les experts dont les figures n'effraient plus</p>
-                  </i>
-                  <a href="offer_details.php?name=Speed" class="btn btn-success">Plus de détails sur cette offre</a>
-                </div>
-              </div>
-            </div> -->
-
-          <!-- </div> -->
-
         </div>
     </section>
 
     <section id="accessories" class="reveal pt-5">
     <div class="container-fluid px-5 pt-5">
       <div class="row px-5 pt-5">
-        <div class="col"><a href="object_details.php"><img class="card-img-top rounded" src="./img/accessory-catalog.jpeg"></a></div>
+        <div class="col acc_img_resize"><a href="object_details.php"><img class="card-img-top rounded" src="./img/accessory-catalog.jpeg"></a></div>
 
         <div class="col pl-4 vertical_center">
           <h2 class="textcolor pb-4">Notre catalogue d'accessoires</h2>
           <div class="col-8 textcolor front_subtitle">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, laudantium quae veritatis laboriosam debitis voluptatem itaque harum ex labore nesciunt deserunt quisquam cupiditate sapiente consequuntur animi dolore molestias illo sint.
+                Lorem Ipsum.
           </div>
 
           <div class="col pt-4"><a href ="catalog.php"><button class="btn btn-success textdark front_subtitle rounded">Consulter le catalogue</button></a></div>
@@ -157,58 +122,61 @@ $nbPacks = count($resultPacks);
     </section>
 
 
-    <section id="about" class="reveal border_bottom pt-5">
 
-    <div class="row px-5 pt-5">
-      <div class="col-3 border_col_left">
-        <div class="card bgfontblack" style="width: 30rem;">
-          <img class="card-img-top rounded" src="./img/about_img1.png" alt="Card image cap" height="500">
-          <div class="card-body">
-            <h2 class="card-title textcolor">Prise en mains facilitée</h2>
-            <i>
-              <p class="card-text textcolor pb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, dolorum quod atque voluptates harum quam soluta, ex hic in veritatis provident reprehenderit expedita magni porro. Tenetur deleniti vitae quasi placeat.</p>
-            </i>
+    <!-- <section id="about" class="reveal border_bottom"> -->
+    <div id="about">
+      <div class="row px-5 py-5 border_bottom">
+        <div class="col border_col_left border_col_right">
+          <div class="card bgfontblack">
+            <img class="card-img-top rounded" src="./img/about_img1.png" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title textcolor">Prise en mains facilitée</h2>
+              <i>
+                <p class="card-text textcolor pb-4">Lorem ipsum</p>
+              </i>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-3 border_col_left">
-        <div class="card bgfontblack" style="width: 30rem;">
-          <img class="card-img-top rounded" src="./img/about_img2.png" alt="Card image cap" height="500">
-          <div class="card-body">
-            <h2 class="card-title textcolor">Service client qualitatif</h2>
-            <i>
-              <p class="card-text textcolor pb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, dolorum quod atque voluptates harum quam soluta, ex hic in veritatis provident reprehenderit expedita magni porro. Tenetur deleniti vitae quasi placeat.</p>
-            </i>
+        <div class="col border_col_left border_col_right">
+          <div class="card bgfontblack">
+            <img class="card-img-top rounded" src="./img/about_img2.png" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title textcolor">Service client qualitatif</h2>
+              <i>
+                <p class="card-text textcolor pb-4">Lorem ipsum.</p>
+              </i>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div class="col-3 border_col_left">
-        <div class="card bgfontblack" style="width: 30rem;">
-          <img class="card-img-top rounded" src="./img/about_img3.jpg" alt="Card image cap" height="500">
-          <div class="card-body">
-            <h2 class="card-title textcolor">Clientèle satisfaite</h2>
-            <i>
-              <p class="card-text textcolor pb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, dolorum quod atque voluptates harum quam soluta, ex hic in veritatis provident reprehenderit expedita magni porro. Tenetur deleniti vitae quasi placeat.</p>
-            </i>
+        
+        <div class="col border_col_left border_col_right">
+          <div class="card bgfontblack">
+            <img class="card-img-top rounded" src="./img/about_img3.jpg" alt="Card image cap" >
+            <div class="card-body">
+              <h2 class="card-title textcolor">Clientèle satisfaite</h2>
+              <i>
+                <p class="card-text textcolor pb-4">Lorem Ipsum.</p>
+              </i>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-3 border_col_left border_col_right">
-        <div class="card bgfontblack" style="width: 30rem;">
-          <img class="card-img-top rounded" src="./img/about_img4.png" alt="Card image cap" height="500">
-          <div class="card-body">
-            <h2 class="card-title textcolor">Pour votre sécurité</h2>
-            <i>
-              <p class="card-text textcolor pb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, dolorum quod atque voluptates harum quam soluta, ex hic in veritatis provident reprehenderit expedita magni porro. Tenetur deleniti vitae quasi placeat.</p>
-            </i>
+        <div class="col border_col_left border_col_right">
+          <div class="card bgfontblack">
+            <img class="card-img-top rounded" src="./img/about_img4.png" alt="Card image cap">
+            <div class="card-body">
+              <h2 class="card-title textcolor">Pour votre sécurité</h2>
+              <i>
+                <p class="card-text textcolor pb-4">Lorem ipsum.</p>
+              </i>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    </section>
+
+    <!-- </section> -->
 
     
     <?php include "struct/footer/footer.php" ?>
@@ -216,5 +184,6 @@ $nbPacks = count($resultPacks);
 </body>
 
 <script src = "backOffice/JS/loader.js"></script>
+
 
 </html>
