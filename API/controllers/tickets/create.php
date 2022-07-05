bce996258bd595101f93a8a005a715f74f7cb240
BLANDIN Arthur <blandin.arthur20@gmail.com>
2022-07-05 14:36:06 +0200

    Ajout d'un ticket en BDD grâce à l'API

--- API/controllers/tickets/create.php
+++ API/controllers/tickets/create.php
@@ -2,11 +2,12 @@
 
 require __DIR__ . "/../../connexion_mysql/json-response.php";
 require __DIR__ . "/../../models/ticket.php";
+require __DIR__ . "/../../connexion_mysql/request.php";
 
 try {
-
-    $tickets = TicketModel::getList($bdd);
-    Response::json(200, [], [ "ticket" => $tickets ]);
+    $json = Request::getJsonBody();
+    TicketModel::create($json,$bdd);
+    Response::json(201, [], [ "success" => true ]);
 } catch (PDOException $exception) {
     $errorMessage = $exception->getMessage();
     Response::json(500, [], [ "error" => "Error while accessing the database: $errorMessage" ]);
--- API/models/ticket.php
+++ API/models/ticket.php
@@ -14,6 +14,32 @@ class TicketModel
 
         return $tickets;
     }
+
+    public static function create($json,$bdd) {
+        $connection = $bdd;
+        $date_now = date('Y-m-d');
+        if (preg_match('/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/', $date_now)) {
+            if(strstr($json['request_type'], 'Bug') != null){
+            $query = $connection->prepare("INSERT INTO iw22_ticket(title, description, status, request_type, priority_level, date_created) VALUES(:title, :description, 'En cours', :request_type, 'Moyen', '".$date_now."');");
+            $query->execute($json);
+            }
+            if(strstr($json['request_type'], 'Panne') != null){
+                $query = $connection->prepare("INSERT INTO iw22_ticket(title, description, status, request_type, priority_level, date_created) VALUES(:title, :description, 'En cours', :request_type, 'Faible', '".$date_now."');");
+                $query->execute($json);
+            }
+            if(strstr($json['request_type'], 'Maintenance') != null){
+                $query = $connection->prepare("INSERT INTO iw22_ticket(title, description, status, request_type, priority_level, date_created) VALUES(:title, :description, 'En cours', :request_type, 'Forte', '".$date_now."');");
+                $query->execute($json);
+            }
+            if(strstr($json['request_type'], 'Bug') == null && strstr($json['request_type'], 'Panne') == null && strstr($json['request_type'], 'Maintenance') == null){
+                echo("Erreur : Le type de requête n'est pas reconnu. Veuillez réessayer plus tard.");
+            }
+
+        } else {
+            textalert("Erreur : Le format de la date n'est pas correct.\nElle doit être au format (YYYY-MM-DD).");
+        }
+
+    }
 }
 
 ?>
